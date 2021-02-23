ce978f4a322c7064bf0032d026c44c777600c86a
paulords95 <paulords95@gmail.com>
2021-02-23 12:25:15 -0300

    optimize

--- index.js
+++ index.js
@@ -15,71 +15,83 @@ setInterval(async () => {
   const sellPrice = parseFloat(result.data.ask);
   const buyPrice = parseFloat(result.data.bid);
   const avgPrice = (buyPrice + sellPrice) / 2;
+
   pool.query(
     `INSERT INTO "dax-api"."DOGE_PRICE" (recent_prices) VALUES($1)`,
-    [parseInt(avgPrice)],
+    [avgPrice.toFixed(5)],
     (error, results) => {
       if (error) {
         throw error;
       }
     }
   );
-}, 30000);
+}, 350000);
 
 app.get("/walletbalance", async (req, res) => {
   const data = await getRequestToAPI("/v1/account/getBalance");
   res.send(data);
 });
 
-
 const getPriceOfCurrency = async (asset) => {
-  const result = []
-  let total = 0
+  const result = [];
+  let total = 0;
   for (let i of asset) {
     if (i.balance > 0 && i.currency != "BRL") {
-      const apiCallResponse = await fetch(`https://api.novadax.com/v1/market/ticker?symbol=${i.currency}_BRL`)
-      const priceCurrency = await apiCallResponse.json()
+      const apiCallResponse = await fetch(
+        `https://api.novadax.com/v1/market/ticker?symbol=${i.currency}_BRL`
+      );
+      const priceCurrency = await apiCallResponse.json();
       if (priceCurrency.data != null) {
-        const cashBalance = parseFloat(i.balance) * parseFloat(priceCurrency.data.ask)
-        total += parseFloat(cashBalance.toFixed(2))
+        const cashBalance =
+          parseFloat(i.balance) * parseFloat(priceCurrency.data.ask);
+        total += parseFloat(cashBalance.toFixed(2));
         result.push({
           balance: i.balance,
           price: priceCurrency.data.lastPrice,
           balanceInFiat: cashBalance.toFixed(2),
           currency: i.currency,
-        })
-        
+        });
       }
-      
     }
   }
 
-  const response = []
+  const response = [];
 
   for (let j of result) {
     response.push({
       balance: j.balance,
-      price:j.price,
+      price: j.price,
       balanceInFiat: j.balanceInFiat,
       currency: j.currency,
-      percentage: (parseFloat(j.balanceInFiat).toFixed(1) / parseFloat(total).toFixed(1) * 100).toFixed(1)
-    })
+      percentage: (
+        (parseFloat(j.balanceInFiat).toFixed(1) /
+          parseFloat(total).toFixed(1)) *
+        100
+      ).toFixed(1),
+    });
   }
 
-  response.push( {
-    totalAssets: total.toFixed(2)
-  })
-  return (response)
-}
+  response.push({
+    totalAssets: total.toFixed(2),
+  });
+  return response;
+};
 
 app.get("/walletbalancepercentage", async (req, res) => {
- try {
-  const data = await getRequestToAPI("/v1/account/getBalance");
-  const stats = await getPriceOfCurrency(data.data)
-  res.send(stats)
- } catch (error) {
-   console.log(error)
- }
+  try {
+    const data = await getRequestToAPI("/v1/account/getBalance");
+
+    const asset = [];
+    for (let i of data.data) {
+      if (i.balance > 0) {
+        asset.push(i);
+      }
+    }
+    const stats = await getPriceOfCurrency(asset);
+    res.send(stats);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/wallethistory", async (req, res) => {
@@ -89,17 +101,9 @@ app.get("/wallethistory", async (req, res) => {
 
 app.get("/recentprices", async (req, res) => {
   const result = await pool.query(
-    'SELECT id, recent_prices, timestamp FROM "dax-api"."DOGE_PRICE" order by id desc limit 340'
+    'SELECT id, recent_prices, timestamp FROM "dax-api"."DOGE_PRICE" order by id desc limit 150'
   );
-  const response = []
-  for (let i of result.rows) {
-    response.push({
-      id: i.id,
-      recent_prices: parseInt(i.recent_prices),
-      timestamp: i.timestamp
-    })
-  }
-  res.json(response);
+  res.json(result.rows);
 });
 
 app.listen(process.env.PORT, () => {
